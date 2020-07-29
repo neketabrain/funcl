fe42b3f0debed98ec4650fa194d1001d4ba6e872
Nikita Karapuzov <karapuzov.nikita@gmail.com>
2020-07-30 01:59:52 +0700

    fix jsdoc

--- src/compose.ts
+++ src/compose.ts
@@ -4,7 +4,6 @@ import { Fn } from "./types";
  * @description Performs right-to-left function composition. The last argument may have any arity; the remaining arguments must be unary.
  * @param {...Function} fns - The functions to compose
  * @return {Function}
- * @sig ((y -> z), (x -> y), ..., (o -> p), ((a, b, ..., n) -> o)) -> ((a, b, ..., n) -> z)
  * @example
  * const upper = (str: string) => str.toUpperCase();
  * const add = (str: string) => str + " world!";
--- src/pipe.ts
+++ src/pipe.ts
@@ -4,7 +4,6 @@ import { Fn } from "./types";
  * @description Performs left-to-right function composition. The first argument may have any arity; the remaining arguments must be unary.
  * @param {...Function} fns - The functions to compose
  * @return {Function}
- * @sig ((y -> z), (x -> y), ..., (o -> p), ((a, b, ..., n) -> o)) -> ((a, b, ..., n) -> z)
  * @example
  * const upper = (str: string) => str.toUpperCase();
  * const add = (str: string) => str + " world!";
